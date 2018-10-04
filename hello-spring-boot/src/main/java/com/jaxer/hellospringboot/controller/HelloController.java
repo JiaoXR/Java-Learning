2f8b12c3da345d26bdc7804c69a8c250f2e712d6
jiaoxiangru <jiaoxiangru@gmail.com>
2018-10-04 18:27:59 +0800

    Thymeleaf

--- hello-spring-boot/src/main/java/com/jaxer/hellospringboot/controller/HelloController.java
+++ hello-spring-boot/src/main/java/com/jaxer/hellospringboot/controller/HelloController.java
@@ -1,17 +1,32 @@
 package com.jaxer.hellospringboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Created by jaxer on 22/09/2018
  */
-@RestController
+//@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String helloWorld() {
         return "Hello, SpringBoot!";
     }
 
+    /**
+     * 使用 Thymeleaf 模板
+     *
+     * @return 模板文件
+     */
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "success~");
+        return "success";
+    }
 }
