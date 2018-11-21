a45a6992e57b93fee10f61670a97946fbb701dcf
jiaoxiangru <jiaoxiangru@gmail.com>
2018-11-22 00:52:57 +0800

    pic

---
+++ spring-boot-advanced/amqp-rabbitmq/src/main/java/com/jaxer/example/amqp/AmqpRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.jaxer.example.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpRabbitmqApplication.class, args);
+	}
+}
---
+++ spring-boot-advanced/amqp-rabbitmq/src/main/java/com/jaxer/example/amqp/domain/Person.java
@@ -0,0 +1,7 @@
+package com.jaxer.example.amqp.domain;
+
+/**
+ * Created by jaxer on 2018/11/22
+ */
+public class Person {
+}
