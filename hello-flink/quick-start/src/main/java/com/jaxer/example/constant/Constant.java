af39c855e3a924bc177eaff41dedb562bbf62e4e
jiaoxiangru <jiaoxiangru@souche.com>
2019-01-22 13:54:40 +0800

    add pics

---
+++ hello-flink/quick-start/src/main/java/com/jaxer/example/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.jaxer.example.constant;
+
+/**
+ * Created by jiaoxiangru on 12:56 PM 2019/1/22
+ */
+public class Constant {
+}
---
+++ hello-flink/quick-start/src/main/java/com/jaxer/example/util/KafkaUtil.java
@@ -0,0 +1,7 @@
+package com.jaxer.example.util;
+
+/**
+ * Created by jiaoxiangru on 12:45 PM 2019/1/22
+ */
+public class KafkaUtil {
+}
