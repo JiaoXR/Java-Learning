272dc1298eb8e9f58cbd8e4a410040737e12ddc8
jiaoxiangru <jiaoxiangru@souche.com>
2019-01-22 14:00:32 +0800

    delete

--- hello-flink/quick-start/src/main/java/com/jaxer/example/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.jaxer.example.constant;
-
-/**
- * Created by jiaoxiangru on 12:56 PM 2019/1/22
- */
-public class Constant {
-}
--- hello-flink/quick-start/src/main/java/com/jaxer/example/util/KafkaUtil.java
+++
@@ -1,7 +0,0 @@
-package com.jaxer.example.util;
-
-/**
- * Created by jiaoxiangru on 12:45 PM 2019/1/22
- */
-public class KafkaUtil {
-}
